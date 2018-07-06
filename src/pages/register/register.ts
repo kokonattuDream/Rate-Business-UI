9d9075542bf11ee875af9f5c4d04c3607ddfdd55
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-06 11:20:55 -0400

    added signup page

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-register',
@@ -15,11 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
+  fullname: string;
+  email: string;
+  password: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  loginPage(){
+    this.navCtrl.setRoot("LoginPage");
   }
 
 }
