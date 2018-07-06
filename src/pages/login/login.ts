b53ba18e2310d610edc21e1720c15f8d99d07d75
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-06 11:10:16 -0400

    styled login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  email: string;
+  password: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  registerPage(){
+    this.navCtrl.push("RegisterPage");
   }
 
 }
