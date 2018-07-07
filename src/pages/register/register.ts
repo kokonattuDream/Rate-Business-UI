46de19ac4d4afca2050d5143c7ad9dd9611ac2a7
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-07 14:18:12 -0400

    loading mark

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { RegisterProvider } from './../../providers/register/register';
 
 @IonicPage()
@@ -13,7 +13,11 @@ export class RegisterPage {
   email: string;
   password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private reg: RegisterProvider) {
+  loading: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private reg: RegisterProvider, public loadingCtrl: LoadingController) {
+
   }
 
   ionViewDidLoad() {
@@ -25,9 +29,31 @@ export class RegisterPage {
   }
 
   userSignup(){
+    this.showLoading();
     this.reg.registerUser(this.fullname, this.email, this.password).subscribe(res => {
-      console.log(res)
-    })
+      this.loading.dismiss();
+
+      if(res.user){
+        this.navCtrl.setRoot("HomePage");
+      }
+
+      if(res.error){
+        console.log(res.error);
+      }
+    });
+
+    this.fullname = '';
+    this.password = '';
+    this.email = '';
   }
 
+
+  showLoading(){
+    this.loading = this.loadingCtrl.create({
+      content: 'Authenticating...',
+      duration: 3000
+    });
+
+    this.loading.present();
+  }
 }
