aaa1fb4353e7cc56e9464eed7901e2763b680604
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-11 19:05:58 -0400

    logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,4 +53,9 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  logout(){
+    this.storage.remove('useremail');
+    this.nav.setRoot("LoginPage");
+  }
 }
