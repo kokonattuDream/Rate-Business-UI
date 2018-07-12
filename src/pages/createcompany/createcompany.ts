b6933ccbcfb5f42b9e29eddbdf688dd42cd4bb90
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-11 20:48:07 -0400

    create a company

--- src/pages/createcompany/createcompany.ts
+++ src/pages/createcompany/createcompany.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CompanyProvider } from '../../providers/company/company';
 
 /**
  * Generated class for the CreatecompanyPage page.
@@ -15,11 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreatecompanyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  name: string;
+  address: string;
+  city: string;
+  country: string;
+  sector: string;
+  website: string;
+  userId: any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private company: CompanyProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatecompanyPage');
   }
 
+  register(){
+    this.company.createCompany(this.name, this.address, this.city, this.country, this.sector, this.website, this.userId)
+    .subscribe(res => {
+      
+    })
+  }
 }
