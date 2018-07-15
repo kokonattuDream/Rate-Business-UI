50198063861027f681f5e3b96044b246cc605ba1
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-15 11:30:50 -0400

    create a custom component for rating stars

---
+++ src/components/newrating/newrating.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'newrating',
+  templateUrl: 'newrating.html'
+})
+export class NewratingComponent {
+
+  @Input() ratenumber: number;
+
+  constructor() {
+  }
+
+}
--- src/pages/companyprofile/companyprofile.module.ts
+++ src/pages/companyprofile/companyprofile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CompanyprofilePage } from './companyprofile';
+import { ComponentsModule } from './../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CompanyprofilePage } from './companyprofile';
   ],
   imports: [
     IonicPageModule.forChild(CompanyprofilePage),
+    ComponentsModule
   ],
 })
 export class CompanyprofilePageModule {}
