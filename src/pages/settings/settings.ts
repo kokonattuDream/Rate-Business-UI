bff5277e3590f5e55aaba2d2fbc6b78b8c03441a
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-07-21 09:30:41 -0400

    camera optiona

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -11,11 +11,13 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class SettingsPage {
 
   user: any;
+  imagePath: string;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private company: CompanyProvider) {
+    private company: CompanyProvider,
+    private camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -30,7 +32,26 @@ export class SettingsPage {
   }
 
   addImage(){
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: false,
+      correctOrientation: true,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 300,
+      targetHeight: 300
+    }
 
+    this.camera.getPicture(options).then((imgUrl => {
+      this.imagePath='data:image/jpeg;base64,' + imgUrl;
+      
+      this.company.uploadImage(this.user, this.imagePath)
+        .subscribe(res=>{
+          
+        });
+    }));
   }
 
 }
